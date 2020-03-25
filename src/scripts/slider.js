a0ad42c58f8f0f7332d27449d7e4c83a9fc9f8c7
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-03-25 22:06:12 +0200

    implemented slider dotss

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,25 +1,60 @@
 const slider = document.querySelector('.slider');
+const slides = Array.from(slider.children);
+const dotsContainer = document.querySelector('.testimonials-dots');
 
 let current = slider.querySelector('.current') || slider.firstElementChild;
 let prev = current.previousElementSibling || slider.lastElementChild;
 let next = current.nextElementSibling || slider.firstElementChild;
 
+slides.forEach(() => {
+  const dot = document.createElement('div');
+  dot.classList.add('testimonials-dot');
+  dotsContainer.append(dot);
+});
+
+const dots = dotsContainer.querySelectorAll('.testimonials-dot');
+
+function activeDot() {
+  const activeSlide = slides.indexOf(document.querySelector('.current'));
+  dots.forEach(e => e.classList.remove('dot-active'));
+  dots[activeSlide].classList.add('dot-active');
+}
+
 function applyClasses() {
-  current.classList.add('current');
   prev.classList.add('prev');
+  current.classList.add('current');
   next.classList.add('next');
 }
 
-function move() {
+function removeClasses() {
   prev.classList.remove('prev');
   current.classList.remove('current');
   next.classList.remove('next');
+}
+
+function move() {
+  removeClasses();
   [prev, current, next] = [current, next, next.nextElementSibling || slider.firstElementChild];
   applyClasses();
+  activeDot();
 }
 
 applyClasses();
+activeDot();
 
 setInterval(() => {
   move();
-}, 3000);
+}, 10000);
+
+dotsContainer.addEventListener('click', e => {
+  if (e.target.matches('.dot-active')) return;
+  if (e.target.matches('.testimonials-dot')) {
+    removeClasses();
+    current = slides[Array.from(dots).indexOf(e.target)];
+    prev = current.previousElementSibling || slider.lastElementChild;
+    next = current.nextElementSibling || slider.firstElementChild;
+    current.classList.add('current');
+    setTimeout(applyClasses, 500);
+    activeDot();
+  }
+});
