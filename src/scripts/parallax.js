a6e520318daf6f46e74bf29baa17bde431cfb4d4
Kirill Arkhypov <k.arkhypov@gmail.com>
2020-03-31 00:22:02 +0300

    parallax script minor fix

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -5,9 +5,8 @@ window.addEventListener('scroll', () => {
   const y = window.scrollY;
   if (y < header.clientHeight) {
     parallax.forEach(i => {
-      const item = i;
       const speed = parseFloat(i.getAttribute('data-speed'));
-      item.style.transform = `translateY(${y * -speed}px)`;
+      i.style.transform = `translateY(${y * -speed}px)`;
     });
   }
 });
