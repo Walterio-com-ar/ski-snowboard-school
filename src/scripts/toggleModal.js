d0b205e97cae2893de72c824d776681d6883c329
Kirill Arkhypov <k.arkhypov@gmail.com>
2021-03-29 13:42:06 +0300

    fixed animation of modal window

--- src/scripts/toggleModal.js
+++ src/scripts/toggleModal.js
@@ -1,3 +1,13 @@
+const modalOverlays = document.querySelectorAll(
+  '.modal-overlay, .modal-submitted, .modal-submitted__result'
+);
+
+setTimeout(() => {
+  modalOverlays.forEach((e) => {
+    e.style.transition = 'all 0.5s';
+  });
+}, 1500);
+
 export function open(e) {
   return e.classList.add('modal-show');
 }
